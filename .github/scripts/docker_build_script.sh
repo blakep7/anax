282c067285cf6e09ddfca10aec5096d0e7fc9b02
blakep7 <pearsonblake9@gmail.com>
2023-05-26 16:46:51 -0700

    Anax Build Test Update, make rpm, mac, docker

--- .github/scripts/docker_build_script.sh
+++ .github/scripts/docker_build_script.sh
@@ -1,18 +1,18 @@
-export arch=${ARCHITECTURE}
-export IMAGE_REPO=$IMAGE_REPO
-export ANAX_IMAGE_VERSION=$ANAX_IMAGE_VERSION
-export CSS_IMAGE_VERSION=$CSS_IMAGE_VERSION
-export ESS_IMAGE_VERSION=$ESS_IMAGE_VERSION
+# export arch=${ARCHITECTURE}
+# export IMAGE_REPO=$IMAGE_REPO
+# export ANAX_IMAGE_VERSION=$ANAX_IMAGE_VERSION
+# export CSS_IMAGE_VERSION=$CSS_IMAGE_VERSION
+# export ESS_IMAGE_VERSION=$ESS_IMAGE_VERSION
 
 # IMAGE_OVERRIDE tells Anax Makefile not to push images, we'll handle it here
 export IMAGE_OVERRIDE="true"
-if ${ARCHITECTURE == 'amd64'} || ${ARCHITECTURE == 'ppc64el'} || ${ARCHITECTURE == 'arm64'}; then
+if ${arch} == 'amd64' || ${arch} == 'ppc64el' || ${arch} == 'arm64'; then
     make fss-package
 fi
-if ${ARCHITECTURE == 'amd64'}; then
+if ${arch} == 'amd64'; then
     make agbot-package
 fi
-if ${ARCHITECTURE == 'arm64'}; then
+if ${arch} == 'arm64'; then
     export USE_DOCKER_BUILDX=true
     # setup the QEMU simulator. You can then run `docker buildx ls` to see which platforms are available.
     docker run --rm --privileged $IMAGE_REPO/multiarch/qemu-user-static:latest --reset -p yes
