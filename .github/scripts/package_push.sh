aed9a81b8f62b5767b446b1b99a77edd9bbf841b
blakep7 <pearsonblake9@gmail.com>
2023-05-31 21:13:14 -0700

    Updated perms of the new shell script to allow execute

--- .github/scripts/package_push.sh
+++ .github/scripts/package_push.sh
@@ -1,33 +1,46 @@
 #!/bin/bash
 
-# Make temp Dockerfiles
+# Deal with Debian Package First
+# Make the Dockerfile for the debs only tarball image
 touch Dockerfile.debs.tarball
 echo "FROM scratch" >> Dockerfile.debs.tarball
 echo "ADD ./debs.tar.gz" >> Dockerfile.debs.tarball
 
-touch Dockerfile.rpm.tarball
-echo "FROM scratch" >> Dockerfile.rpm.tarball
-echo "ADD ./rpm.tar.gz" >> Dockerfile.rpm.tarball
-
-# Make tarballs
+# DEBUG - Remove for production
 echo | pwd
-tar -czvf debs.tar.gz ./pkg/deb/debs/*.deb
-tar -czvf rpm.tar.gz ../rpmbuild/RPMS/x86_64/*.rpm ..rpmbuild/RPMS/ppc64le/*.rpm
 
+# Make debs tarball
+tar -czvf debs.tar.gz ./pkg/deb/debs/*.deb
+
+# DEBUG - Remove for production
 ls -la
 
-# Build and push images
+# Build docker image with only debs tarball
 docker build \
     --no-cache \
     -t $IMAGE_REPO/anax_debian:$ANAX_IMAGE_VERSION \
     -f Dockerfile.debs.tarball \
     .
     
+# Push docker image
+docker push anax_debian:$ANAX_IMAGE_VERSION
+
+# Deal with RPM Package
+if [[ ${arch} == 'amd64' || ${arch} == 'ppc64el' ]]; then
+
+touch Dockerfile.rpm.tarball
+
+echo "FROM scratch" >> Dockerfile.rpm.tarball
+echo "ADD ./rpm.tar.gz" >> Dockerfile.rpm.tarball
+
+tar -czvf rpm.tar.gz ../rpmbuild/RPMS/x86_64/*.rpm ..rpmbuild/RPMS/ppc64le/*.rpm
+
 docker build \
     --no-cache \
     -t $IMAGE_REPO/anax_rpm:$ANAX_IMAGE_VERSION \
     -f Dockerfile.rpm.tarball \
     .
 
-docker push anax_debian:$ANAX_IMAGE_VERSION
 docker push anax_rpm:$ANAX_IMAGE_VERSION
+
+fi
