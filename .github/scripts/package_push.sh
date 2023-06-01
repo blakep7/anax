cdfb0febf8a4f1bdabcb204d8b618b27ea743ee0
blakep7 <pearsonblake9@gmail.com>
2023-05-31 22:40:56 -0700

    More docker push issues

--- .github/scripts/package_push.sh
+++ .github/scripts/package_push.sh
@@ -10,15 +10,14 @@ echo "ADD ./debs.tar.gz ." >> Dockerfile.debs.tarball
 tar -czvf debs.tar.gz ./pkg/deb/debs/*.deb
 
 # Build docker image with only debs tarball
-docker build \
-    --no-cache \
-    -t $IMAGE_REPO/${arch}_anax_debian:$ANAX_IMAGE_VERSION \
-    -f Dockerfile.debs.tarball \
-    .
-    
+docker build --no-cache -t ${IMAGE_REPO}/${arch}_anax_debian:testing -f Dockerfile.debs.tarball .
+docker images
+
 # Push docker image
-docker tag ${arch}_anax_debian:$ANAX_IMAGE_VERSION ${arch}_anax_debian:$ANAX_IMAGE_VERSION
-docker push ${arch}_anax_debian:$ANAX_IMAGE_VERSION
+docker tag ${IMAGE_REPO}/${arch}_anax_debian:testing ${IMAGE_REPO}/${arch}_anax_debian:${ANAX_IMAGE_VERSION}
+
+docker push ${IMAGE_REPO}/${arch}_anax_debian:testing
+docker push ${IMAGE_REPO}/${arch}_anax_debian:${ANAX_IMAGE_VERSION}
 
 # # Deal with RPM Package
 # if [[ ${arch} == 'amd64' || ${arch} == 'ppc64el' ]]; then
