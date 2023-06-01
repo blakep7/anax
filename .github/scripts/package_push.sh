880d2c0cd036b7bf03d4a4bf21216c526c0412b5
blakep7 <pearsonblake9@gmail.com>
2023-06-01 08:53:47 -0700

    RPM Package errors

--- .github/scripts/package_push.sh
+++ .github/scripts/package_push.sh
@@ -19,22 +19,22 @@ docker tag ${IMAGE_REPO}/${arch}_anax_debian:testing ${IMAGE_REPO}/${arch}_anax_
 docker push ${IMAGE_REPO}/${arch}_anax_debian:testing
 docker push ${IMAGE_REPO}/${arch}_anax_debian:${ANAX_IMAGE_VERSION}
 
-# # Deal with RPM Package
-# if [[ ${arch} == 'amd64' || ${arch} == 'ppc64el' ]]; then
+# Deal with RPM Package
+if [[ ${arch} == 'amd64' || ${arch} == 'ppc64el' ]]; then
 
-# touch Dockerfile.rpm.tarball
+touch Dockerfile.rpm.tarball
 
-# echo "FROM scratch" >> Dockerfile.rpm.tarball
-# echo "ADD ./rpm.tar.gz ." >> Dockerfile.rpm.tarball
+echo "FROM scratch" >> Dockerfile.rpm.tarball
+echo "ADD ./rpm.tar.gz ." >> Dockerfile.rpm.tarball
 
-# tar -czvf rpm.tar.gz ../rpmbuild/RPMS/x86_64/*.rpm ..rpmbuild/RPMS/ppc64le/*.rpm
+tar -czvf rpm.tar.gz ../rpmbuild/RPMS/x86_64/*.rpm ..rpmbuild/RPMS/ppc64le/*.rpm
 
-# docker build \
-#     --no-cache \
-#     -t $IMAGE_REPO/${arch}_anax_rpm:$ANAX_IMAGE_VERSION \
-#     -f Dockerfile.rpm.tarball \
-#     .
+docker build \
+    --no-cache \
+    -t $IMAGE_REPO/${arch}_anax_rpm:$ANAX_IMAGE_VERSION \
+    -f Dockerfile.rpm.tarball \
+    .
 
-# docker push ${arch}_anax_rpm:$ANAX_IMAGE_VERSION
+docker push ${arch}_anax_rpm:$ANAX_IMAGE_VERSION
 
-# fi
+fi
