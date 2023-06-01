426934cfeac6aa6cb7443950290f62b011318f5b
blakep7 <pearsonblake9@gmail.com>
2023-05-31 22:11:46 -0700

    More docker push issues

--- .github/scripts/package_push.sh
+++ .github/scripts/package_push.sh
@@ -6,14 +6,8 @@ touch Dockerfile.debs.tarball
 echo "FROM scratch" >> Dockerfile.debs.tarball
 echo "ADD ./debs.tar.gz ." >> Dockerfile.debs.tarball
 
-# DEBUG - Remove for production
-echo | pwd
-
 # Make debs tarball
 tar -czvf debs.tar.gz ./pkg/deb/debs/*.deb
-
-# DEBUG - Remove for production
-ls -la
 
 # Build docker image with only debs tarball
 docker build \
@@ -23,24 +17,25 @@ docker build \
     .
     
 # Push docker image
+docker tag ${arch}_anax_debian:$ANAX_IMAGE_VERSION ${arch}_anax_debian:$ANAX_IMAGE_VERSION
 docker push ${arch}_anax_debian:$ANAX_IMAGE_VERSION
 
-# Deal with RPM Package
-if [[ ${arch} == 'amd64' || ${arch} == 'ppc64el' ]]; then
+# # Deal with RPM Package
+# if [[ ${arch} == 'amd64' || ${arch} == 'ppc64el' ]]; then
 
-touch Dockerfile.rpm.tarball
+# touch Dockerfile.rpm.tarball
 
-echo "FROM scratch" >> Dockerfile.rpm.tarball
-echo "ADD ./rpm.tar.gz ." >> Dockerfile.rpm.tarball
+# echo "FROM scratch" >> Dockerfile.rpm.tarball
+# echo "ADD ./rpm.tar.gz ." >> Dockerfile.rpm.tarball
 
-tar -czvf rpm.tar.gz ../rpmbuild/RPMS/x86_64/*.rpm ..rpmbuild/RPMS/ppc64le/*.rpm
+# tar -czvf rpm.tar.gz ../rpmbuild/RPMS/x86_64/*.rpm ..rpmbuild/RPMS/ppc64le/*.rpm
 
-docker build \
-    --no-cache \
-    -t $IMAGE_REPO/${arch}_anax_rpm:$ANAX_IMAGE_VERSION \
-    -f Dockerfile.rpm.tarball \
-    .
+# docker build \
+#     --no-cache \
+#     -t $IMAGE_REPO/${arch}_anax_rpm:$ANAX_IMAGE_VERSION \
+#     -f Dockerfile.rpm.tarball \
+#     .
 
-docker push ${arch}_anax_rpm:$ANAX_IMAGE_VERSION
+# docker push ${arch}_anax_rpm:$ANAX_IMAGE_VERSION
 
-fi
+# fi
