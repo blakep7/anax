1fce648020802ff3bf7deacb5a92db2ef871c270
blakep7 <pearsonblake9@gmail.com>
2023-06-01 09:11:35 -0700

    RPM Package errors

--- .github/scripts/package_push.sh
+++ .github/scripts/package_push.sh
@@ -10,8 +10,11 @@ echo "ADD ./debs.tar.gz ." >> Dockerfile.debs.tarball
 tar -czvf debs.tar.gz ./pkg/deb/debs/*.deb
 
 # Build docker image with only debs tarball
-docker build --no-cache -t ${IMAGE_REPO}/${arch}_anax_debian:testing -f Dockerfile.debs.tarball .
-docker images
+docker build \
+    --no-cache \
+    -t ${IMAGE_REPO}/${arch}_anax_debian:testing \
+    -f Dockerfile.debs.tarball \
+    .
 
 # Push docker image
 docker tag ${IMAGE_REPO}/${arch}_anax_debian:testing ${IMAGE_REPO}/${arch}_anax_debian:${ANAX_IMAGE_VERSION}
@@ -31,10 +34,13 @@ tar -czvf rpm.tar.gz ../rpmbuild/RPMS/x86_64/*.rpm ..rpmbuild/RPMS/ppc64le/*.rpm
 
 docker build \
     --no-cache \
-    -t $IMAGE_REPO/${arch}_anax_rpm:$ANAX_IMAGE_VERSION \
+    -t $IMAGE_REPO/${arch}_anax_rpm:testing \
     -f Dockerfile.rpm.tarball \
     .
 
-docker push ${arch}_anax_rpm:$ANAX_IMAGE_VERSION
+docker tag ${IMAGE_REPO}/${arch}_anax_rpm:testing ${IMAGE_REPO}/${arch}_anax_debian:${ANAX_IMAGE_VERSION}
+
+docker push ${IMAGE_REPO}/${arch}_anax_rpm:testing
+docker push ${IMAGE_REPO}/${arch}_anax_rpm:${ANAX_IMAGE_VERSION}
 
 fi
