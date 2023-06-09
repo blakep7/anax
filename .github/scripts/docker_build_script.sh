f73887870ad79b335e9605dd33a55737aebcdfb1
blakep7 <pearsonblake9@gmail.com>
2023-06-08 17:21:55 -0700

    Update docker_build_script.sh

--- .github/scripts/docker_build_script.sh
+++ .github/scripts/docker_build_script.sh
@@ -9,7 +9,7 @@ if [[ ${arch} == 'amd64' ]]; then
 fi
 
 # What does this do?
-if [[ ${arch} == 'arm64' || ${arch} == 'ppc64el']]; then
+if [[ ${arch} == 'arm64' || ${arch} == 'ppc64el' ]]; then
     export USE_DOCKER_BUILDX=true
     # # setup the QEMU simulator. You can then run `docker buildx ls` to see which platforms are available.
     # docker run --rm --privileged $IMAGE_REPO/multiarch/qemu-user-static:latest --reset -p yes
