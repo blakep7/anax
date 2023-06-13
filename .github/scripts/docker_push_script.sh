2da62b950c7416d4b61e201ad8594fd01efed2a7
blakep7 <pearsonblake9@gmail.com>
2023-06-13 16:52:12 -0700

    Fixed docker push script

--- .github/scripts/docker_push_script.sh
+++ .github/scripts/docker_push_script.sh
@@ -1,10 +1,10 @@
 # Names of the images created for each architecture
 if [[ ${arch} == 'amd64' ]]; then
-    images = ['amd64_agbot', 'amd64_anax', 'amd64_anax_k8s', 'amd64_auto-upgrade-cronjob_k8s', 'amd64_cloud-sync-service', 'amd64_edge-sync-service']
+    images=('amd64_agbot' 'amd64_anax' 'amd64_anax_k8s' 'amd64_auto-upgrade-cronjob_k8s' 'amd64_cloud-sync-service' 'amd64_edge-sync-service')
 elif [[ ${arch} == 'ppc64el' ]]; then
-    images = ['ppc64el_anax', 'ppc64el_anax_k8s', 'ppc64el_auto-upgrade-cronjob_k8s', 'ppc64el_edge-sync-service']
+    images=('ppc64el_anax' 'ppc64el_anax_k8s' 'ppc64el_auto-upgrade-cronjob_k8s' 'ppc64el_edge-sync-service')
 elif [[ ${arch} == 'arm64' ]]; then
-    images = ['arm64_anax', 'arm64_anax_k8s', 'arm64_auto-upgrade-cronjob_k8s', 'arm64_edge-sync-service']
+    images=('arm64_anax' 'arm64_anax_k8s' 'arm64_auto-upgrade-cronjob_k8s' 'arm64_edge-sync-service')
 fi
 
 # Push those images
