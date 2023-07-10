1710448d1c093cc3c84993d29889611570d2eacb
blakep7 <pearsonblake9@gmail.com>
2023-07-10 13:18:48 -0700

    Update perms

--- .github/scripts/promote_images_script.sh
+++ .github/scripts/promote_images_script.sh
@@ -21,7 +21,6 @@ images=('amd64_agbot' 'amd64_anax' 'amd64_anax_k8s' 'amd64_auto-upgrade-cronjob_
 
 for image in "${images[@]}"; do
     docker pull ${FROM_REGISTRY}/${image}:${FROM_TAG}
-    echo ${image}
     for tag in ${TO_TAGS[@]}; do
         docker tag ${FROM_REGISTRY}/${image}:${FROM_TAG} ${TO_REGISTRY}/${image}:${tag}
         docker push ${TO_REGISTRY}/${image}:${tag}
