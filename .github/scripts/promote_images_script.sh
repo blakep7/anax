422c059f2cc002806f0a3864ecd89a5cb9a61732
blakep7 <pearsonblake9@gmail.com>
2023-07-10 11:14:56 -0700

    Update promote_images_script.sh

--- .github/scripts/promote_images_script.sh
+++ .github/scripts/promote_images_script.sh
@@ -18,7 +18,7 @@ if [[ -z "$TO_TAGS" ]]; then
 fi
 
 images=('amd64_agbot' 'amd64_anax' 'amd64_anax_k8s' 'amd64_auto-upgrade-cronjob_k8s' 'amd64_cloud-sync-service' 'amd64_edge-sync-service' 'ppc64el_anax' 'ppc64el_anax_k8s' 'ppc64el_auto-upgrade-cronjob_k8s' 'ppc64el_edge-sync-service' 'arm64_anax' 'arm64_anax_k8s' 'arm64_auto-upgrade-cronjob_k8s' 'arm64_edge-sync-service')
-tags=${TO_TAGS}
+tags=(${TO_TAGS})
 
 for image in "${images[@]}"; do
     # docker pull ${FROM_REGISTRY}/${image}:${FROM_TAG}
