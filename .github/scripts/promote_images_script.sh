5896e8ee6f37647068984f20622f72c1b40dd108
blakep7 <pearsonblake9@gmail.com>
2023-07-10 13:51:29 -0700

    Update

--- .github/scripts/promote_images_script.sh
+++ .github/scripts/promote_images_script.sh
@@ -21,6 +21,11 @@ images=('amd64_agbot' 'amd64_anax' 'amd64_anax_k8s' 'amd64_auto-upgrade-cronjob_
 
 for image in "${images[@]}"; do
     docker pull ${FROM_REGISTRY}/${image}:${FROM_TAG}
+
+    if [[ ${image} == 'amd64_agbot' ]]; then
+        echo "AGBOT_VERSION="$(docker inspect ${FROM_REGISTRY}/${image}:${FROM_TAG} | jq -r '.[].Config.Labels.version')" >> $GITHUB_OUTPUT
+    fi
+
     for tag in ${TO_TAGS[@]}; do
         docker tag ${FROM_REGISTRY}/${image}:${FROM_TAG} ${TO_REGISTRY}/${image}:${tag}
         docker push ${TO_REGISTRY}/${image}:${tag}
