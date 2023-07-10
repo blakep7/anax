001969200ca3481d8353c129d3a2a4551f8819a3
blakep7 <pearsonblake9@gmail.com>
2023-07-10 11:24:30 -0700

    Update promote_images_script.sh

--- .github/scripts/promote_images_script.sh
+++ .github/scripts/promote_images_script.sh
@@ -20,11 +20,10 @@ fi
 images=('amd64_agbot' 'amd64_anax' 'amd64_anax_k8s' 'amd64_auto-upgrade-cronjob_k8s' 'amd64_cloud-sync-service' 'amd64_edge-sync-service' 'ppc64el_anax' 'ppc64el_anax_k8s' 'ppc64el_auto-upgrade-cronjob_k8s' 'ppc64el_edge-sync-service' 'arm64_anax' 'arm64_anax_k8s' 'arm64_auto-upgrade-cronjob_k8s' 'arm64_edge-sync-service')
 
 for image in "${images[@]}"; do
-    # docker pull ${FROM_REGISTRY}/${image}:${FROM_TAG}
+    docker pull ${FROM_REGISTRY}/${image}:${FROM_TAG}
     echo ${image}
     for tag in ${TO_TAGS[@]}; do
-        echo ${tag}
-        # docker tag ${FROM_REGISTRY}/${image}:${FROM_TAG} ${TO_REGISTRY}/${image}:${tag}
-        # docker push ${TO_REGISTRY}/${image}:${tag}
+        docker tag ${FROM_REGISTRY}/${image}:${FROM_TAG} ${TO_REGISTRY}/${image}:${tag}
+        docker push ${TO_REGISTRY}/${image}:${tag}
     done
 done
