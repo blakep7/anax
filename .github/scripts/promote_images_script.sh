50b9a8fc3aaf035a8a52af8c79f9919c58337c07
blakep7 <pearsonblake9@gmail.com>
2023-07-10 10:53:40 -0700

    Update

--- .github/scripts/promote_images_script.sh
+++ .github/scripts/promote_images_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-images=('amd64_agbot' 'amd64_anax' 'amd64_anax_k8s' 'amd64_auto-upgrade-cronjob_k8s' 'amd64_cloud-sync-service' 'amd64_edge-sync-service', 'ppc64el_anax' 'ppc64el_anax_k8s' 'ppc64el_auto-upgrade-cronjob_k8s' 'ppc64el_edge-sync-service', 'arm64_anax' 'arm64_anax_k8s' 'arm64_auto-upgrade-cronjob_k8s' 'arm64_edge-sync-service')
+images=('amd64_agbot' 'amd64_anax' 'amd64_anax_k8s' 'amd64_auto-upgrade-cronjob_k8s' 'amd64_cloud-sync-service' 'amd64_edge-sync-service' 'ppc64el_anax' 'ppc64el_anax_k8s' 'ppc64el_auto-upgrade-cronjob_k8s' 'ppc64el_edge-sync-service' 'arm64_anax' 'arm64_anax_k8s' 'arm64_auto-upgrade-cronjob_k8s' 'arm64_edge-sync-service')
 
 if [[ -z "$FROM_REGISTRY" ]]; then
     echo "::error::Script Variable FROM_REGISTRY was not set"
@@ -20,6 +20,9 @@ fi
 
 for image in "${images[@]}"; do
     docker pull ${FROM_REGISTRY}/${image}:${FROM_TAG}
-    docker tag ${FROM_REGISTRY}/${image}:${FROM_TAG} ${TO_REGISTRY}/${image}:${TO_TAG}
-    docker push ${TO_REGISTRY}/${image}:${TO_TAG}
+
+    for tag in "${TO_TAGS[@]}"; do
+        docker tag ${FROM_REGISTRY}/${image}:${FROM_TAG} ${TO_REGISTRY}/${image}:${tag}
+        docker push ${TO_REGISTRY}/${image}:${tag}
+    done
 done
